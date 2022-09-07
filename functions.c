be615bcada37ecbf90220b3d68ba1e398cbfff1e
Sheila Kahwai <SheilaKahwai@users.noreply.github.com>
2022-09-07 02:53:16 -0700

    create push and pall functions

--- functions.c
+++ functions.c
@@ -1,2 +1,33 @@
 #include "monty.h"
 
+void push_to_stack(stack_t **new_node, __attribut__((unused))unsigned int line_num)
+{
+  stack_t *temp;
+
+  if (new_node == NULL || *new_node == NULL)
+    exit(EXIT_FAILURE);
+  if (head == NULL)
+    {
+      head = *new_node;
+      return;
+    }
+  temp = head;
+  head = *new_node;
+  head->next = temp;
+  temp->prev = head;
+}
+
+void display(stack_t **stack, unsigned int line_num)
+{
+  stack_t *temp;
+
+  (void) line_num;
+  if (stack == NULL)
+    exit(EXIT_FAILURE);
+  temp = *stack;
+  while (temp != NULL)
+    {
+      printf("%d\n", temp->n);
+      temp = temp->next;
+    }
+}
